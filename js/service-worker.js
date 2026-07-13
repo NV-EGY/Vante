4730aaf21b40c75504c62402265b1a2ee5b66411
N.V <taswekshope@gmail.com>
2026-07-13 22:21:42 +0300

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,6 +1,16 @@
+// service-worker.js
 self.addEventListener('install', event => {
     event.waitUntil(self.skipWaiting());
 });
+
 self.addEventListener('activate', event => {
     event.waitUntil(self.clients.claim());
+});
+
+// استقبال الإشعارات من الصفحة وعرضها
+self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'SHOW_NOTIFICATION') {
+        const { title, options } = event.data.payload;
+        self.registration.showNotification(title, options);
+    }
 });
